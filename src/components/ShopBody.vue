ea94d3ebb6647ff991964efefa745674667fba79
6210110145 <6210110145@psu.ac.th>
2024-07-26 15:35:59 +0700

    train axios

--- src/components/ShopBody.vue
+++ src/components/ShopBody.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-    <h1> สินค้าทั้งหมด </h1>
+    <h1 class="d-flex flex-column justify-space-between align-center mt-3 mb-3"> สินค้าทั้งหมด </h1>
     <v-row>
         <v-col col="3" v-for="(item, index) in product" :key="index">
-            <v-card width="350">
+            <v-card
+            width="350"
+            class="ml-2">
                 <v-img 
                 src="https://static.independent.co.uk/2021/05/24/20/doge.jpg"
                 width="350"
-                height="300"/>
+                height="300">
+                </v-img>
                 <v-card-title primary-title> 
+                    xxx Bath
+                </v-card-title>
+                <v-card-title> 
                      {{item.name}}
                 </v-card-title>
-                    <!-- <v-card-actions>
-                        <v-btn color="info" @click="displayAlertParam(item.name)">alert</v-btn>
-                    </v-card-actions> -->
+                <v-card-subtitle>
+                    รายละเอียดสินค้า
+                </v-card-subtitle>
+                    <v-card-actions>
+                        <v-btn
+                        text
+                        color="info"
+                        >add cart</v-btn>
+                    </v-card-actions>
             </v-card>
         </v-col>
     </v-row>
--- src/views/Apicon.vue
+++ src/views/Apicon.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1> APIcon</h1>
+    <v-row>
+        <v-col cols="12">
+            <v-btn color="success" @click="newItem()">netItem</v-btn>
+        </v-col>
+    </v-row>
     <!-- <body-1> {{apidata}} </body-1> -->
     <v-row>
         <v-col col="3" v-for="(item, index) in apidata" :key="index">
@@ -12,9 +17,10 @@
                 <v-card-title primary-title> 
                     {{item.firstname}} {{item.surname}} 
                 </v-card-title>
-                    <!-- <v-card-actions>
-                        <v-btn color="info" @click="displayAlertParam(item.name)">alert</v-btn>
-                    </v-card-actions> -->
+                    <v-card-actions>
+                        <v-btn color="info" @click="editItem(item)">edit</v-btn>
+                        <v-btn color="" text @click="deleteData(item)"> delete </v-btn>
+                    </v-card-actions>
             </v-card>
         </v-col>
 
@@ -46,7 +52,7 @@
         max-width="500px">
             <v-card>
                 <v-card-title>
-                    Account
+                    {{savemode}}
                 </v-card-title>
                 <v-card-text>
                     <v-row>
@@ -102,8 +108,24 @@
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="error" text> cancel</v-btn>
-                    <v-btn color="success" text> save </v-btn>
+                    <v-btn color="error" text @click="closeItem()"> cancel</v-btn>
+                    <v-btn color="success" text @click="saveSelect()"> save </v-btn>
+                    <!-- <v-btn color="" text @click="deleteData()"> delete </v-btn> -->
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <v-dialog
+        v-model="dialogdelete"
+        max-width="500px">
+            <v-card>
+                <v-card-title>
+                    Sure?
+                </v-card-title>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="error" text @click="closeItem()"> NO </v-btn>
+                    <v-btn color="success" text @click="deleteData()"> YES </v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -114,7 +136,8 @@
 export default {
     data() {
         return {
-            dialogedit: true,
+            dialogedit: false,
+            dialogdelete: false,
             id: '',
             apidata: [],
             postdata: { // ชุดไว้ส่งข้อมูล
@@ -140,7 +163,47 @@ export default {
     created() {
         this.getData()
     },
+    computed: {
+        savemode() {
+            return this.id == '' ? 'New User' : 'Edit User'
+        }
+    },
     methods: {
+        saveSelect() {
+            if(this.id != '') {
+                this.savePutData()
+                // alert('put')
+            }else {
+                this.savePostData()
+                // alert('post')
+            }
+        },
+        deleteSelect() {
+            if (this.id) {
+                this.dialogdelete = true
+            }
+        },
+        newItem() {
+            this.id = ''
+            this.postdata = {...this.postdefault}
+            this.dialogedit = true
+        },
+        editItem(item) {
+            this.id = item._id
+            this.postdata = {...item}
+            this.dialogedit = true
+        },
+        deleteItem(item) {
+            this.id = item._id
+            this.postdata = {...item}
+            this.dialogdelete = true
+        },
+        closeItem() {
+            this.id = ''
+            this.postdata = {...this.postdefault}
+            this.dialogedit = false
+            this.dialogdelete = false
+        },
         getData() {
             this.axios.get('http://localhost:3000/users/', {
                 headers: {
@@ -150,9 +213,58 @@ export default {
                 console.log(response.data)
                 this.apidata = response.data.data // ทำให้เป็น array
             })
-        }
+        },
+        async savePostData() {
+            try {
+                const {data} = await this.axios.post('http://localhost:3000/users/register', this.postdata, {
+                    headers: {
+                        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmIkMTAkazU1WUdzcVdsei82aENiVU9USzFyLnZTZmJRelouSktVOHdSQU1VQzRNdmhJZVdQYi5UcG0iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MjE5NjU5Nzd9.cI1D5-9Tk9vgje9GFwGlJ9SrnfKHZUCOErREDC-40ng`
+                    },
+                })
+                console.log(data)
+                alert('register complete')
+                this.getData()
+                this.closeItem()
+            }catch (err) {
+                console.log(err)
+                alert(err)
+            }
+        },
+        async savePutData() {
+            try {
+                const {data} = await this.axios.put('http://localhost:3000/users/'+this.id, this.postdata, {
+                    headers: {
+                        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmIkMTAkazU1WUdzcVdsei82aENiVU9USzFyLnZTZmJRelouSktVOHdSQU1VQzRNdmhJZVdQYi5UcG0iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MjE5NjU5Nzd9.cI1D5-9Tk9vgje9GFwGlJ9SrnfKHZUCOErREDC-40ng`
+                    },
+                })
+                console.log(data)
+                alert('update complete')
+                this.getData()
+                this.closeItem()
+            }catch (err) {
+                console.log(err)
+                alert(err)
+            }
+        },
+        async deleteData(item) {
+            if(confirm('delete ' + item.username)){
+                try {
+                    const {data} = await this.axios.delete('http://localhost:3000/users/'+item._id, {
+                        headers: {
+                            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmIkMTAkazU1WUdzcVdsei82aENiVU9USzFyLnZTZmJRelouSktVOHdSQU1VQzRNdmhJZVdQYi5UcG0iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MjE5NjU5Nzd9.cI1D5-9Tk9vgje9GFwGlJ9SrnfKHZUCOErREDC-40ng`
+                        },
+                    })
+                    console.log(data)
+                    alert('delete complete')
+                    this.closeItem()
+                    this.getData()
+                }catch (err) {
+                    console.log(err)
+                    alert(err)
+                }
+            }   
+        },
     }
-    
 }
 </script>
 
