c0348b0a478d2a929749b72d7af32d4d4a822962
Teerapat Photongkam <teerapat132012@gmail.com>
2024-08-15 17:16:29 +0700

    profile

--- src/views/ShopProfile.vue
+++ src/views/ShopProfile.vue
@@ -22,22 +22,19 @@
                     <v-list dense>
                     <v-list-item>
                         <v-list-item-content>
-                        <v-list-item-title>Name:</v-list-item-title>
-                        <v-list-item-subtitle> {{ profile.firstname }} {{ profile.surname }} </v-list-item-subtitle>
+                            <v-list-item-title>Name: {{ profile.firstname }} {{ profile.surname }}</v-list-item-title>
                         </v-list-item-content>
                     </v-list-item>
 
                     <v-list-item>
                         <v-list-item-content>
-                        <v-list-item-title>Age:</v-list-item-title>
-                        <v-list-item-subtitle> {{ profile.age }} </v-list-item-subtitle>
+                            <v-list-item-title>Age: {{ profile.age }}</v-list-item-title>
                         </v-list-item-content>
                     </v-list-item>
 
                     <v-list-item>
                         <v-list-item-content>
-                        <v-list-item-title>Gender:</v-list-item-title>
-                        <v-list-item-subtitle> {{ profile.gender }} </v-list-item-subtitle>
+                            <v-list-item-title>Gender: {{ profile.gender }}</v-list-item-title>
                         </v-list-item-content>
                     </v-list-item>
                     </v-list>
@@ -45,61 +42,106 @@
 
                 <v-card-actions>
                     <v-btn color="primary" @click="editProfileDialog = true">Edit Profile</v-btn>
-                    <v-btn color="error" @click="logout">Logout</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="error" @click="passwordChangeDialog = true">change password</v-btn>
                 </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
   </v-container>
 
-  <v-dialog
-    v-model="editProfileDialog"
-    max-width="500px"
-  >
-    <v-card>
-        <v-card-title primary-title>
-            Edit Profile
-        </v-card-title>
-        <v-card-text>
-            <v-row>
-                <v-col cols="2">
-                    <v-select
-                        name="role"
-                        id="role"
-                        :items="roles"
-                        label="Standard"
-                        v-model="profile.role"
-                    ></v-select>
-                </v-col>
-                <v-col cols="5">
-                    <v-text-field
-                        name="username"
-                        label="username"
-                        id="username"
-                        v-model="profile.username"
-                    ></v-text-field>
-                </v-col>
-                <v-col cols="5">
-                    <v-text-field
-                       :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                        :rules="[rules.required, rules.min]"
-                        :type="show1 ? 'text' : 'password'"
-                        name="password"
-                        label="password"
-                        id="password"
-                        hint="At least 8 characters"
-                        counter
-                        v-model="profile.password"
-                        @click:append="show1 = !show1"
-                    ></v-text-field>
-                </v-col>
-            </v-row>
-        </v-card-text>
-        <v-card-actions>
-            <v-btn text color="error" @click="editProfileDialog = false"> cancel </v-btn>
-        </v-card-actions>
-    </v-card>
-  </v-dialog>
+    <v-dialog
+        v-model="editProfileDialog"
+        max-width="500px"
+    >
+        <v-card>
+            <v-card-title primary-title>
+                Edit Profile
+            </v-card-title>
+            <v-card-text>
+                <v-row>
+                    <v-col cols="12">
+                        <v-text-field
+                            name="username"
+                            label="username"
+                            id="username"
+                            v-model="profile.username"
+                        ></v-text-field>
+                    </v-col>
+                    <!-- <v-col cols="6">
+                        <v-text-field
+                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                            :rules="[rules.required, rules.min]"
+                            :type="show1 ? 'text' : 'password'"
+                            name="password"
+                            label="password"
+                            id="password"
+                            hint="At least 8 characters"
+                            counter
+                            v-model="profile.password"
+                            @click:append="show1 = !show1"
+                        ></v-text-field>
+                    </v-col> -->
+                    <v-col cols="12">
+                        <v-text-field
+                            name="firstname"
+                            label="firstname"
+                            id="firstname"
+                            v-model="profile.firstname"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-text-field
+                            name="surname"
+                            label="surname"
+                            id="surname"
+                            v-model="profile.surname"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field
+                            name="age"
+                            label="age"
+                            id="age"
+                            v-model="profile.age"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field
+                            name="gender"
+                            label="gender"
+                            id="gender"
+                            v-model="profile.gender"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-autocomplete
+                            v-model="profile.role"
+                            :items="roles"
+                            dense
+                            filled
+                            label="roles"
+                        ></v-autocomplete>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn text color="success" @click="editProfile()"> edit </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn text color="error" @click="editProfileDialog = false"> cancel </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+
+    <v-dialog
+        v-model="passwordChangeDialog"
+    >
+        <v-card>
+            <v-card-title primary-title>
+                Change Password
+            </v-card-title>
+        </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -111,7 +153,13 @@ export default {
             profile: {},
             show1: false,
             roles: ['admin', 'user'],
-            editProfileDialog: false
+            // rules: {
+            //     required: value => !!value || 'Required.',
+            //     min: v => v.length >= 2 || 'Min 8 characters',
+            //     emailMatch: () => (`The email and password you entered don't match`),
+            // },
+            editProfileDialog: false,
+            passwordChangeDialog: false,
         }
     },
     created() {
@@ -129,6 +177,21 @@ export default {
                     this.profile = res.data.data
                 })
             }catch(err) {
+                alert(err)
+            }
+        },
+        editProfile() {
+            try {
+                this.axios.put("http://localhost:3000/users/"+this.profile._id, this.profile, {
+                    headers: {
+                        Authorization: `Bearer ${this.$cookies.get("token")}`
+                    }
+                }).then((res) => {
+                    console.log(res.data.data)
+                    this.editProfileDialog = false
+                    alert(res.data.message)
+                })
+            }catch (err) {
                 alert(err)
             }
         }
